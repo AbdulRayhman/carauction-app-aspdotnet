85083afbda7fcbda0254b02840b881e6305d1f36
Abdul Rehman <s.abderrehman@gmail.com>
2024-02-12 20:33:11 +0500

    my initial

---
+++ src/AuctionService/Entities/Auction.cs
@@ -0,0 +1,16 @@
+namespace AuctionService.Entites;
+public class Auction
+{
+  public Guid Id {get; set;}
+
+  public int ReservePrice { get; set; }
+  public string Seller { get; set; } = "";
+  public string Winner { get; set; } = "";
+  public int? SoldAmount { get; set; }
+  public int? CurrentHighBid { get; set; }
+  public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+  public DateTime UpdateAt { get; set; } = DateTime.UtcNow;
+  public DateTime AuctionAt { get; set; }
+  public Status Status { get; set; }
+  public Item Item { get; set; }
+}
